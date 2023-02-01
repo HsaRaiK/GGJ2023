0545eb07c8f7807037cdaab7fc981987661e42c8
Ahmet Yunus Turna <ahmetyunusturna@hotmail.com>
2023-02-01 21:43:39 +0300

    Deneme

---
+++ Assets/Scripts/PlayerSwitch.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSwitch : MonoBehaviour 
+{
+    private void Start() {
+        
+    }
+    
+    private void OnCollisionEnter(Collision other) {
+        
+    }
+}
