c3e342c072624f18b79dff9429289bab5b910e90
Ahmet Yunus Turna <ahmetyunusturna@hotmail.com>
2023-02-01 23:40:46 +0300

    CartCurt

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,10 @@ public class PlayerMovement : MonoBehaviour
     
     [SerializeField] float horizontalSpeed = 5f;
     [SerializeField] float jumpPower = 5f;
+    public static float movement;
+    private int doubleJumpCounter = 2;
+    private bool grounded = true;
+
 
     private void Start()
     {
@@ -20,27 +24,67 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         CharacterMovement();
-        
-            
-        
     }
 
 
 
     private void CharacterMovement()
     {
-        var movement = Input.GetAxis("Horizontal");
+        movement = Input.GetAxis("Horizontal");
       
         transform.position += new Vector3(movement , 0 , 0) * horizontalSpeed/1000;
 
-        if(Input.GetKeyDown(KeyCode.Space) == true){
-
-            rb.velocity = new Vector2(movement , jumpPower);
+        if(gameObject.CompareTag("Ninja")){
+            DoubleJump();
+            
         }
+        else if(gameObject.CompareTag("Kid")){
+            Jump();
+        }
+
+
 
 
 
         
         
     }
+
+
+    private void Jump()
+    {
+        
+
+        if(Input.GetKeyDown(KeyCode.Space) == true && grounded == true )
+        {
+
+            rb.velocity = new Vector2(movement , jumpPower);
+            grounded = false;
+            
+        }
+
+    }
+
+    private void DoubleJump(){
+        
+
+        if(Input.GetKeyDown(KeyCode.Space) == true & doubleJumpCounter > 0)
+        {
+
+            rb.velocity = new Vector2(movement , jumpPower);
+            doubleJumpCounter = doubleJumpCounter -1;
+            
+        }
+
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+      if(col.gameObject.tag == "Ground"){
+        doubleJumpCounter = 2;
+        grounded = true;
+      }   
+    }
+
+    
+    
 }
--- Assets/Scripts/PlayerSwitch.cs
+++ Assets/Scripts/PlayerSwitch.cs
@@ -1,15 +1,55 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSwitch : MonoBehaviour 
+public class PlayerSwitch : MonoBehaviour
 {
-    private void Start() {
-        
+    [SerializeField] GameObject child;
+    [SerializeField] GameObject ninja;
+
+    [SerializeField] float switchTime = 5f;
+
+    [SerializeField] float switchCoolDown = 5f;
+    private bool runSwitch = true;
+    void Start()
+    {
+        ninja.SetActive(false);
+        runSwitch = true;
     }
+
     
-    private void OnCollisionEnter(Collision other) {
+    void Update()
+    {
         
+        if(Input.GetKey(KeyCode.R) & runSwitch == true)
+        {
+            StartCoroutine(BackSwitch());
+        }
+    }
+
+    void ChildToNinja(){
+        ninja.transform.position = child.transform.position;
+        child.SetActive(false);
+        ninja.SetActive(true);
+
+    }
+    void NinjaToChild()
+    {
+        child.transform.position = ninja.transform.position;
+        child.SetActive(true);
+        ninja.SetActive(false);
+
+    }
+    IEnumerator BackSwitch(){
+        runSwitch = false;
+        ChildToNinja();
+        yield return new WaitForSeconds(switchTime);
+        NinjaToChild();
+        yield return new WaitForSeconds(switchCoolDown);
+        runSwitch = true;
+
+        
+
+
     }
 }
