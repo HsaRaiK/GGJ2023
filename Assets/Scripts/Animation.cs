f84d8fbc09460e6564a874ea49389d053579c200
Ahmet Yunus Turna <ahmetyunusturna@hotmail.com>
2023-02-02 03:36:21 +0300

    Animasyon cart curt

--- Assets/Scripts/Animation.cs
+++ Assets/Scripts/Animation.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Animation : MonoBehaviour
 {
     
-    
+    Rigidbody2D rb;
     Animator kidRun;
+    Animator kidJump;
+    Animator kidCrouch;
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         kidRun = GetComponent<Animator>();
+        kidJump = GetComponent<Animator>();
+        kidCrouch = GetComponent<Animator>();
 
     }
 
@@ -21,6 +26,23 @@ public class Animation : MonoBehaviour
         else
         {
             kidRun.SetBool("Run" , false);
+        }
+        if(rb.velocity.y > 0){
+            kidJump.SetInteger("JumpOrFall" , 1);
+
+        }
+        else if(rb.velocity.y < 0 ){
+            kidJump.SetInteger("JumpOrFall" , -1);
+        }
+        else{
+            kidJump.SetInteger("JumpOrFall" , 0);
+        }
+        if(Input.GetKey(KeyCode.DownArrow)){
+            kidCrouch.SetBool("Crouch" , true);
+
+        }
+        else{
+            kidCrouch.SetBool("Crouch" , false);
         }
     }
 }
